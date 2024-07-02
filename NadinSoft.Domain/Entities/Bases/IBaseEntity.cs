d907cedf2ea7e4d35b38ba69c567a217019ee5c9
Mehran Derisavi <mehran.devops@gmail.com>
2024-07-02 19:23:58 +0330

    Implement BaseEntity And ProductEntity

---
+++ NadinSoft.Domain/Entities/Bases/IBaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace NadinSoft.Domain.Entities
+{
+    public interface IBaseEntity<Tkey>
+    {
+        Tkey Id { get; set; }
+
+        DateTime CreatedAt { get; set; }
+        DateTime? ModifiedAt{ get; set; }
+        DateTime? RemovedAt{ get; set; }
+    }
+}
---
+++ NadinSoft.Domain/Entities/Products/Product.cs
@@ -0,0 +1,18 @@
+﻿
+namespace NadinSoft.Domain.Entities.Products
+{
+    public class Product : IBaseEntity<int>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string ManufacturePhone { get; set; }
+        public string ManufactureEmail { get; set; }
+        public DateTime ProductDate { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+        public DateTime? RemovedAt { get; set; }
+    }
+}
